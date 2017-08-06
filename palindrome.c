a55024c63ea5049b7b2ec08a29c1561c5888aec9
surya241 <30549483+surya241@users.noreply.github.com>
2017-08-06 18:36:43 +0530

    palindrome.c

---
+++ palindrome.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main()
+{
+   int n,num,rev=0;
+   printf("enter the n value:");
+   scanf("%d", &n);
+   num=n;
+   while(n!=0)
+   {
+      rev=(rev*10)+(n%10);
+      n=n/10;
+   } 
+   if(rev==num) 
+      printf("%d is a palindrome number",num);
+   else
+      printf("%d is not a palindrome number",num);
+   }
