4775412aa7cfdff6b2261f679852eada352deaaa
surya241 <30549483+surya241@users.noreply.github.com>
2017-08-08 16:07:40 +0530

    odd.c
     odd number

--- odd.c
+++ odd.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
+#include<conio.h>
 void main()
 {
 int m,n,i;
+  clrscr();
 printf("enter the intervale:");
 scanf("%d%d",&m,&n);
 for(i=m;i<=n;i++)
@@ -11,4 +13,5 @@ if((i%2)!=0)
 printf("%d\n",i);
 }
 }
+  getch();
 }
