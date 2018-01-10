8723cb7d1e4a7dc7e1978d9ef716d530546b2f57
surya241 <30549483+surya241@users.noreply.github.com>
2018-01-10 16:12:55 +0530

    time.c
    time

--- time.c
+++ time.c
@@ -6,6 +6,6 @@ printf("enter the time in minutes:");
 scanf("%f",&min);
 printf("\n");
 hour=min/60;
-printf("the given time in  %fmin %fhr",min,hour);
+printf("the given time in  %fhr %min",hour,min);
 return 0;
 }
