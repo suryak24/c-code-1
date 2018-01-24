8a190a6b13c34fbaaca8a48066e66040512beeac
surya241 <30549483+surya241@users.noreply.github.com>
2018-01-24 15:58:56 +0530

    Update 26.c

--- 26.c
+++ 26.c
@@ -1,23 +1,24 @@
 #include<stdio.h>
 int main()
 {
-int a[20],i,j,swap,v;
+int a[20],i,j,temp,k;
 scanf("%d",&v);
-for(i=0;i<v;i++)
+for(i=0;i<k;i++)
 {
 scanf("%d",&a[i]);
 }
-for(i=0;i<v;i++)
+for(i=0;i<k;i++)
 {
 if(a[i]>a[j])
 {
-swap=a[i];
+temp=a[i];
 a[i]=a[j];
-a[j]=swap;
+a[j]=temp;
 }
 }
-for(i=0;i<v;i++)
+for(i=0;i<k;i++)
 {
 printf("%d',a[i]);
 }
+       return 0;
 }
