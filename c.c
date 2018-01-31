87ba21b83256cb5bf2e5b448182d94474d81fd5c
surya241 <30549483+surya241@users.noreply.github.com>
2018-01-31 16:03:00 +0530

    Create c.c

---
+++ c.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+char s[100],i,count,n;
+printf("Enter the string ");
+scanf("%s",s[i]);
+for(i=0;s[i]!=NULL;i++)
+{
+if(s[i]>='0'&&s[i]<='9')
+{
+    count++;
+}
+}
+printf("%d",count);
+return 0;
+}
