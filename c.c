2e7b2de5c0a11e2e11863328698577ace173be97
surya241 <30549483+surya241@users.noreply.github.com>
2018-01-31 16:06:11 +0530

    Update c.c

--- c.c
+++ c.c
@@ -1,12 +1,15 @@
 #include<stdio.h>
+#include<string.h>
 int main()
 {
 char s[100],i,count,n;
+printf("enter the limit\n");
+scanf("%d",&n);
 printf("Enter the string ");
 scanf("%s",s[i]);
-for(i=0;s[i]!=NULL;i++)
+for(i=0;s[i]<n;i++)
 {
-if(s[i]>='0'&&s[i]<='9')
+if(s[i]>='0' && s[i]<='9')
 {
     count++;
 }
