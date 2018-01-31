59b7c054b7cc9de69af43d66c4c559b07b7285a0
surya241 <30549483+surya241@users.noreply.github.com>
2018-01-31 16:48:44 +0530

    Create line.c

---
+++ line.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+char str[200];
+int count= 0, i;
+printf("Enter the String\n");
+scanf("%[^\n]s",str);
+for (i = 0;str[i]!=0;i++)
+{
+if (str[i] =='\n')
+count++; 
+}
+printf("number of lines in given paragraph  %d\n", count);
+return 0;
+}
