8a97ffc111fde0c7672dac2b477bc545984a4d8b
surya241 <30549483+surya241@users.noreply.github.com>
2018-02-02 16:01:10 +0530

    Create print.c

---
+++ print.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+char s[100];
+int n,i,j;
+    printf("enter the string:\n");
+    scanf("%s",s[j]);
+    printf("Enter the n values:");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        printf("%s\n",s[j]);
+    }
+   return 0; 
+}
