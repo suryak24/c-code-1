052489563e78e743ab27fc880a9900fa10a545c4
surya241 <30549483+surya241@users.noreply.github.com>
2018-02-02 16:36:11 +0530

    Create range.c

---
+++ range.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+clrscr();
+printf("Enetr the n value");
+scanf("%d",&n);
+if(n>=1 && n<=10)
+{
+printf("yes");
+}
+else
+{
+printf("No");
+}
+getch();
+}
