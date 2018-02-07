de0a081b1a851cb2c21a89b2f6d1c7e75d5c05d7
surya241 <30549483+surya241@users.noreply.github.com>
2018-02-07 16:18:27 +0530

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+clrscr();
+printf(enter the value:");
+scanf("%d",&n);
+if(n%2==0)
+{
+printf("the given number is even");
+}
+else
+{
+printf("the given number is odd");
+}
+getch();
+}
